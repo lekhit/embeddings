e842950ead13392783c59b5d1ee46dd738616cd6
lekhit borole <blekhit@gmail.com>
2023-07-15 22:44:56 +0530

    added basic code

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-from api.index import app
+from sentence_transformers import SentenceTransformer
+from fastapi import FastAPI
+
+import json
+encoder = SentenceTransformer('all-MiniLM-L6-v2')
+print(encoder.encode('hello world'))
+app = FastAPI()
+
+
+@app.get("/")
+async def root(query:str='hello world'):
+    return {"message": json.dumps(encoder.encode(query).tolist())}
+    return {'m':"aasf"}
+
+
